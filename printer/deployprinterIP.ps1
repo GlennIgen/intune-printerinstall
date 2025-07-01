276cf9b0c331f283940f6260f2136e597620d331
glennigen <64850623+GlennIgen@users.noreply.github.com>
2025-07-01 09:07:06 +0200

    Update deployprinterIP.ps1

--- printer/deployprinterIP.ps1
+++ printer/deployprinterIP.ps1
@@ -4,22 +4,25 @@ $printers = @(
     @{ Name = "Printer 1";     IP = "192.168.0.10";  Driver = "Printer Driver 1"; PortName = "port_printer_Printer1" },
     @{ Name = "Printer 2";     IP = "192.168.0.11";  Driver = "Printer Driver 2"; PortName = "port_printer_Printer2" }
 )
-$printersOK = $true
+$printersOK = $null
+$printerPortsOK = $null
 # Install Printer port
 foreach ($printer in $printers) {
     if (-not (Get-PrinterPort -Name $printer.PortName -ErrorAction SilentlyContinue)) {
         try {
             Add-PrinterPort -Name $printer.PortName -PrinterHostAddress $printer.IP
+            $printerPortsOK = $true
         } catch {
             Write-Host "Error installing printer port: $($printer.PortName)"
-            $printersOK = $false
+            $printerPortsOK = $false
         }
     }
 
 # Add printe with; name, driver and port name
     if (-not (Get-Printer -Name $printer.Name -ErrorAction SilentlyContinue)) {
         try {
-            Add-Printer -Name $printer.Name -DriverName $printer.Driver -PortName $printer.PortName -ErrorAction Stop
+            Add-Printer -Name $printer.Name -DriverName $printer.Driver -PortName $printer.PortName
+            $printersOK = $true
         } catch {
             Write-Host "Error adding printer: $($printer.Name)"
             $printersOK = $false
@@ -27,7 +30,7 @@ foreach ($printer in $printers) {
     }
 }
 # Completion status
-if ($printersOK) {
+if ($printersOK -and $printerPortsOK) {
     exit 0
 } else {
     exit 1
